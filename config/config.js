fb24efc2c16175a32a09af559a05b3ac1db25d1b
OFMA2022 <ayofe08@gmail.com>
2022-09-23 11:12:50 +0200

    Actualizar la conexion para poder conectar con la base de datos de qovery0

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 export default {
   username: "root",
   password: "iCoQhNF723HJrV-xIvqrqV9EBXXJviQ6",
-  database: "goodhands-bd",
+  database: "goodhands",
   host: "z6140b8b2-mysql.z897bb54d.blockdev.sh",
   dialect: "mysql",
 };
